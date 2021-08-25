8f01493f8d5df555d78dd18067a0be79ca406848
Rukiye Çadırcı <83542142+rukicad@users.noreply.github.com>
2021-08-25 12:15:39 +0300

    Add files via upload
    Homework has been uploaded to the system.

---
+++ posts.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * posts.php
+ *
+ * Bu betik direk olarak erişilebilir. functions.php'de yaptığınız gibi bir
+ * kontrol eklemenize gerek yok.
+ *
+ * Bu betik dosyası içerisinde functions.php'de yer alan fonksiyonları da kullanarak
+ * aşağıdaki işlemleri gerçekleştirmenizi bekliyoruz.
+ *
+ * - functions.php içerisinde oluşturacağınız `getRandomPostCount` fonksiyonunuza min
+ * ve max değerleri gönderip bu fonksiyondan rastgele bir tam sayı elde etmeniz
+ * gerekiyor. (min ve max için istediğiniz değerleri seçebilirsiniz. Random bir
+ * tam sayı elde etmek için `rand` (https://www.php.net/manual/en/function.rand.php)
+ * fonksiyonunu kullanabilirsiniz.)
+ *
+ * - Elde ettiğiniz bu sayıyı da kullanarak `getLatestPosts` fonksiyonunu
+ * çalıştırmalısınız. Bu fonksiyondan aldığınız diziyi kullanarak `post.php` betik
+ * dosyasını döngü içinde dahil etmeli ve her yazı için detayları göstermelisiniz.
+ */
+
+include_once("functions.php"); 
+
+$rand_post_count = getRandomPostCount(1, 15); // 1 ila 15 arasında rasgele integer sayı döndürür
+$posts = getLatestPosts($rand_post_count); // rasgele post sayı değerine göre son postları dizi olarak getirir
+
+foreach ($posts as $id => $post) {
+   include('post.php'); // Geçerli post içeriğini getirir
+}
+ 
+
